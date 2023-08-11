5b3edb47205bff19d62807b1c50c47438d62e2a5
mttptrsn <matthew.s.peterson@gmail.com>
2023-08-10 17:23:37 -0700

    improve xp ui

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -26,8 +26,8 @@ export default function Experience() {
     <div className="flex flex-row justify-center gap-4">
   
       {/* Left Arrow */}
-      <div className={`fixed left-4 top-1/2 transform -translate-y-1/2 ${activeIndex > 0 ? 'cursor-pointer' : 'opacity-30 cursor-default'}`} onClick={activeIndex > 0 ? handleBack : undefined}>
-        <FcPrevious />
+      <div className={`fixed h-full w-[33px] md:w-[50px] md:left-6 lg:w-[100px] lg:left-16 xl:w-[200px] xl:left-18 left-2 top-1/2 transform -translate-y-1/2 ${activeIndex > 0 ? 'cursor-pointer' : 'opacity-30 cursor-default'}`} onClick={activeIndex > 0 ? handleBack : undefined}>
+        <FcPrevious className='flex items-center justify-center h-full w-full'/>
       </div>
   
       {/* Experience Cards */}
@@ -44,8 +44,8 @@ export default function Experience() {
       ))}
   
       {/* Right Arrow */}
-      <div className={`fixed right-4 top-1/2 transform -translate-y-1/2 ${activeIndex < workExperience.length - 1 ? 'cursor-pointer' : 'opacity-30 cursor-default'}`} onClick={activeIndex < workExperience.length - 1 ? handleNext : undefined}>
-        <FcNext />
+      <div className={`fixed right-2 h-full w-[33px] md:w-[50px] md:right-6 lg:w-[100px] lg:right-16 xl:w-[200px] xl:right-18 top-1/2 transform -translate-y-1/2 ${activeIndex < workExperience.length - 1 ? 'cursor-pointer' : 'opacity-30 cursor-default'}`} onClick={activeIndex < workExperience.length - 1 ? handleNext : undefined}>
+        <FcNext className='flex items-center justify-center h-full w-full'/>
       </div>
     </div>
   </section>
